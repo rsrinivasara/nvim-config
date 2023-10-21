2e35a2795d1b67d1a5ec1602e3d6479b55930ab5
Ramachandren Srinivasa Rao <ramachandren@gmail.com>
2023-10-21 13:20:55 -0400

    Fix tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
-
-local opt = vim.opt
-
-opt.tabstop = 4
-opt.expandtab = true
 
